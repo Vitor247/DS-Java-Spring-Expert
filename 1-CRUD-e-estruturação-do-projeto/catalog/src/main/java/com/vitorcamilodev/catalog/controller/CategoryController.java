15efad3b5fcf2975859673d3f6d141745b3b934c
Vitor Camilo <vittor.camilo@gmail.com>
2026-08-12 17:19:17 -0300

    find category by id, exception handling

--- 1-CRUD-e-estruturação-do-projeto/catalog/src/main/java/com/vitorcamilodev/catalog/controller/CategoryController.java
+++ 1-CRUD-e-estruturação-do-projeto/catalog/src/main/java/com/vitorcamilodev/catalog/controller/CategoryController.java
@@ -5,6 +5,7 @@ import com.vitorcamilodev.catalog.entities.Category;
 import com.vitorcamilodev.catalog.services.CategoryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,13 @@ public class CategoryController {
 
     @GetMapping
     public ResponseEntity<List<CategoryDTO>> findAll() {
-        List<CategoryDTO> list = service.findAll();
-        return ResponseEntity.ok(list);
+        List<CategoryDTO> dto = service.findAll();
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+        CategoryDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
     }
 }
--- 1-CRUD-e-estruturação-do-projeto/catalog/src/main/java/com/vitorcamilodev/catalog/services/CategoryService.java
+++ 1-CRUD-e-estruturação-do-projeto/catalog/src/main/java/com/vitorcamilodev/catalog/services/CategoryService.java
@@ -3,10 +3,11 @@ package com.vitorcamilodev.catalog.services;
 import com.vitorcamilodev.catalog.dto.CategoryDTO;
 import com.vitorcamilodev.catalog.entities.Category;
 import com.vitorcamilodev.catalog.repositories.CategoryRepository;
+import com.vitorcamilodev.catalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -17,8 +18,18 @@ public class CategoryService {
         this.repository = repository;
     }
 
+    @Transactional(readOnly = true)
     public List<CategoryDTO> findAll(){
         List<Category> list = repository.findAll();
         return list.stream().map(CategoryDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+
+        Category category = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found"));
+
+        return new CategoryDTO(category);
     }
 }
